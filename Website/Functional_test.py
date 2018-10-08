5696df30ff4677b455a38dee33593fccbcca73ce
Ana María Espinosa <am.espinosa11@uniandes.edu.co>
2018-10-07 20:50:59 -0500

    Paso 1: Titulo busco ayuda(verde)

--- Website/Functional_test.py
+++ Website/Functional_test.py
@@ -1,14 +1,19 @@
-from unittest import TestCase
 from selenium import webdriver
+import unittest
 
-
-class FunctionalTest(TestCase):
+class FunctionalTest(unittest.TestCase):
     def setUp(self):
-        self.browser = webdriver.Chrome("C:\\Selenium\\chromedriver.exe")
+    	self.browser = webdriver.Chrome("C:\\Selenium\\chromedriver.exe")
+    	self.browser.implicitly_wait(2)
 
     def tearDown(self):
         self.browser.quit()
 
-    def test_1_title(self):
-        self.browser.get("http://127.0.0.1:8000")
-        self.assertIn("Busco Ayuda", self.browser.title)
+    def test_title(self):
+        self.browser.get('http://localhost:8000')
+        self.assertIn('Busco Ayuda', self.browser.title)
+
+
+
+if __name__ == '__main__':
+    unittest.main()
--- Website/models.py
+++ Website/models.py
@@ -1,3 +1,27 @@
-from django.db import models
-
 # Create your models here.
+from django.db import models
+from django.contrib.auth.models import User
+
+class TiposDeServicio(models.Model):
+    nombre = models.CharField(max_length=1000)
+    imagen = models.ImageField(upload_to='services')
+
+    def __unicode__(self):
+        return u'{0}'.format(self.nombre)
+
+
+class Trabajador(models.Model):
+    nombre = models.CharField(max_length=1000)
+    apellidos = models.CharField(max_length=1000)
+    aniosExperiencia = models.IntegerField()
+    tiposDeServicio = models.ForeignKey(TiposDeServicio, null=True, on_delete=models.PROTECT)
+    telefono = models.CharField(max_length=1000)
+    correo = models.CharField(max_length=1000)
+    imagen = models.ImageField(upload_to='photos')
+    usuarioId = models.OneToOneField(User, null=True, on_delete=models.PROTECT)
+
+
+class Comentario(models.Model):
+    texto = models.CharField(max_length=1000)
+    trabajador = models.ForeignKey(Trabajador, null=True, on_delete=models.PROTECT)
+    correo = models.CharField(max_length=1000)
